b7a30ee311f7cbe107f6c9a2733fd99505c4a31f
NilsRutgers <46595922+NilsRutgers@users.noreply.github.com>
2024-05-07 16:20:47 +0200

    Update arduino_serial_comm.ino

--- arduino_serial_comm/arduino_serial_comm.ino
+++ arduino_serial_comm/arduino_serial_comm.ino
@@ -45,9 +45,10 @@ void putReceivedDataStringInData(String dirtyString){
   String subString = dirtyString;
   int i = 0;
   while (dirtyString.indexOf("$") >= 0) {
-    data[i++] = dirtyString.substring(0, dirtyString.indexOf("$"));
+    data[i] = dirtyString.substring(0, dirtyString.indexOf("$"));
     dirtyString = dirtyString.substring(dirtyString.indexOf("$") + 1, dirtyString.length());
     Serial.println(dirtyString);
+    i++;
   }
   data[i] = dirtyString;
 }
