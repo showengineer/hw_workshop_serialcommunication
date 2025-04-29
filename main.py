bae623b0d2871d4a714a21690ea01b593856a486
jouleion <124351760+jouleion@users.noreply.github.com>
2025-04-29 15:33:02 +0200

    data changed

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import pandas as pd
 
 # save file in the same folder as the python file.
 # file in the file name you need and add the correct columns.
-file_name = 'CONTINUOUS_TIME_GreenhouseGasses.xlsx'
+file_name = 'edited_data.xlsx'
 columns_name_list = [f'{x}' for x in range(1901, 2018)]
-connected_port = 'COM10'
+connected_port = 'COM6'
 
 # DO NOT TOUCH
 data = pd.read_excel(file_name)
